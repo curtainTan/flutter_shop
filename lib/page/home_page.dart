7d5806b29b518adef7654e08400b79423625553f
curtainTan <1948750473>
2019-03-11 23:27:58 +0800

    15节完成...

--- lib/page/home_page.dart
+++ lib/page/home_page.dart
@@ -35,13 +35,16 @@ class _HomePageState extends State<HomePage> {
             String leaderImage =data['data']['shopInfo']['leaderImage'];
             String leaderPhone = data['data']['shopInfo']['leaderPhone'];
 
+            List<Map> recommendList = (data['data']['recommend'] as List).cast();
+
             return SingleChildScrollView(
               child: Column(
                 children: <Widget>[
                   SwiperDiy(swiperDateList: swiper,),
                   TopNavigator( navigatorList: navigatorList, ),
                   AdBanner( adPicture: advertesPicture, ),
-                  LeaderPhone( leaderImage: leaderImage, leaderPhone: leaderPhone, )
+                  LeaderPhone( leaderImage: leaderImage, leaderPhone: leaderPhone, ),
+                  Recommend(recommendList:recommendList),
                 ],
               )
             );
@@ -179,7 +182,10 @@ class Recommend extends StatelessWidget {
       height: ScreenUtil().setHeight(380),
       margin: EdgeInsets.only(top: 10.0),
       child: Column(
-
+        children: <Widget>[
+          _titleWidget(),
+          _recommedList()
+        ],
       ),
     );
   }
@@ -206,9 +212,45 @@ class Recommend extends StatelessWidget {
   Widget _recommedList(){
 
     return Container(
-       
+       height: ScreenUtil().setHeight(330),
+       child: ListView.builder(
+         scrollDirection: Axis.horizontal,
+         itemCount: recommendList.length,
+         itemBuilder: ( context, index ){
+           return _item(index);
+         },
+       ),
     );
+  }
 
+  Widget _item(index){
+    return InkWell(
+      onTap: (){},
+      child: Container(
+        height: ScreenUtil().setHeight(330),
+        width: ScreenUtil().setWidth(250),
+        padding: EdgeInsets.all(8.0),
+        decoration:BoxDecoration(
+          color:Colors.white,
+          border:Border(
+            left: BorderSide(width:0.5,color:Colors.black12)
+          )
+        ),
+        child: Column(
+          children: <Widget>[
+            Image.network(recommendList[index]['image']),
+            Text('￥${recommendList[index]['mallPrice']}'),
+            Text(
+              '￥${recommendList[index]['price']}',
+              style: TextStyle(
+                decoration: TextDecoration.lineThrough,
+                color:Colors.grey
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 
 
